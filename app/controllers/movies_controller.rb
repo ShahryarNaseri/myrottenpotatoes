fd5da1166925271b8103c4e90070f6e6c9194ce2
Shahryar Naseri <shahryar_n2000@yahoo.com>
2012-06-17 12:22:57 -0700

    Defined the show action in movies_controller.rb

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,3 +2,8 @@
 def index
   @movies = Movie.all
 end
+def show
+  id = params[:id]#retrieve movie ID from URI route
+  @movie = Movie.find(id)#look up movie by unique ID
+  #will render app/views/movies/show.html.haml by default
+end
