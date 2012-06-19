529e1a32454af48d17f2e66d7e86567a63a56b55
Shahryar Naseri <shahryar_n2000@yahoo.com>
2012-06-19 00:32:15 -0700

    Defined the MovieCOntroller calls in movies_controller.rb and added CSS to application.css

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,13 @@
 # This file is app/controllers/movies_controller.rb
-def index
-  @movies = Movie.all
-end
-def show
-  id = params[:id]#retrieve movie ID from URI route
-  @movie = Movie.find(id)#look up movie by unique ID
-  #will render app/views/movies/show.html.haml by default
+class MoviesController < ApplicationController
+
+  def index
+    @movies = Movie.all
+  end
+  def show
+    id = params[:id]#retrieve movie ID from URI route
+    @movie = Movie.find(id)#look up movie by unique ID
+    #will render app/views/movies/show.html.haml by default
+  end
+
 end
