5c7f5b6df6df02dc2a68c0c30be99ddfa69ea63f
Shahryar Naseri <shahryar_n2000@yahoo.com>
2012-06-20 01:32:00 -0700

    Defined the edit and update actions inside the movies_controller.rb

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -20,5 +20,16 @@ class MoviesController < ApplicationController
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
+  
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
 
 end
--- app/controllers/movies_controller.rb~
+++ app/controllers/movies_controller.rb~
@@ -17,6 +17,7 @@ class MoviesController < ApplicationController
   
   def create
     @movie = Movie.create!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
 
