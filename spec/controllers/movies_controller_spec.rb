4b0f55cc2d73d5404de5ea678d760e672f210343
Shahryar Naseri <shahryar_n2000@yahoo.com>
2012-06-24 06:18:43 -0700

    Modified the movies_controller_spec.rb

--- spec/controllers/movies_controller_spec.rb
+++ spec/controllers/movies_controller_spec.rb
@@ -2,8 +2,12 @@ require 'spec_helper'
 
 describe MoviesController do
   describe 'searching TMDb' do
-    it 'should call the model method that performs TMDb search'
-    it 'should select the Search Results template for rendering'
+    it 'should call the model method that performs TMDb search' do
+      pending "Need to choose name for model method"
+    end  
+    it 'should select the Search Results template for rendering' do
+      flunk "No template exist yet"
+    end  
     it 'should make the TMDb search results available to that template'
   end
 end
